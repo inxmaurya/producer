ff87bb0bae9d3ca5ba2177d70d983a6054a440ea
snmmaurya <smspace@gmail.com>
2024-11-22 19:10:29 +0530

    f

--- source/app.ts
+++ source/app.ts
@@ -1,14 +1,34 @@
 import { Kafka } from 'kafkajs';
+import dotenv from 'dotenv';
+// Determine the environment and load the corresponding .env file
+const envFile = `.env.${process.env.NODE_ENV}`;
+
+// Load environment variables from .env file
+dotenv.config({path: envFile});
+
+const BROCKER_LISTS = [
+  process.env.FIRST_BROKER!,
+  process.env.SECOND_BROKER!,
+  process.env.THIRD_BROKER!,
+  process.env.FOURTH_BROKER!,
+  process.env.FIFTH_BROKER!,
+  process.env.SIXTH_BROKER!,
+];
+
+const CONFIGURATIONS = {
+  SSL: (process.env.SSL! === 'true'),
+  SASL: undefined
+};
+
+console.log("------------------------");
+console.log(BROCKER_LISTS);
+console.log("------------------------");
 
 const kafka = new Kafka({
   clientId: 'my-producer',
-  brokers: [
-    'b-1.smc.4nooo6.c2.kafka.ap-south-1.amazonaws.com:9094',
-    'b-2.smc.4nooo6.c2.kafka.ap-south-1.amazonaws.com:9094',
-    'b-3.smc.4nooo6.c2.kafka.ap-south-1.amazonaws.com:9094',
-  ],
-  ssl: true,
-  sasl: undefined, // Set this if SASL is required.
+  brokers: BROCKER_LISTS,
+  ssl: CONFIGURATIONS['SSL'],
+  sasl: CONFIGURATIONS['SASL'], // Set this if SASL is required.
 });
 
 const producer = kafka.producer();
@@ -16,9 +36,7 @@ const producer = kafka.producer();
 async function produceMessage() {
   await producer.connect();
   console.log('Producer connected');
-
   const topic = 'test-topic';
-
   let times = 1
   while (times <= 1000) {
     // Send a message
@@ -30,6 +48,7 @@ async function produceMessage() {
         { key: 'key2', value: 'Message for Partition 3' },
       ],
     });
+    console.log("Sent");
     await sleep(1000);
     times = times + 1;
   }
@@ -42,7 +61,6 @@ produceMessage().catch((err) => {
   console.error('Error in producer:', err);
   producer.disconnect();
 });
-
 
 function sleep(ms: number): Promise<void> {
   return new Promise(resolve => setTimeout(resolve, ms));
